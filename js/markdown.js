66f5ea8a71a861319f8d61745cbb110f7dc321a7
Sebastian Maniak <sebastian@maniak.io>
2026-04-27 14:11:53 -0400

    feat: implement markdown stripping to plain text

--- js/markdown.js
+++ js/markdown.js
@@ -1,3 +1,17 @@
 export function stripMarkdown(text) {
-    return text;
+    return text
+        .replace(/^#{1,6}\s+/gm, '')
+        .replace(/```[\s\S]*?```/g, '')
+        .replace(/`([^`]+)`/g, '$1')
+        .replace(/!\[([^\]]*)\]\([^)]+\)/g, '')
+        .replace(/\[([^\]]+)\]\([^)]+\)/g, '$1')
+        .replace(/(\*\*|__)(.*?)\1/g, '$2')
+        .replace(/(\*|_)(.*?)\1/g, '$2')
+        .replace(/~~(.*?)~~/g, '$1')
+        .replace(/^>\s+/gm, '')
+        .replace(/^[-*+]\s+/gm, '')
+        .replace(/^\d+\.\s+/gm, '')
+        .replace(/^---+$/gm, '')
+        .replace(/\n{3,}/g, '\n\n')
+        .trim();
 }
